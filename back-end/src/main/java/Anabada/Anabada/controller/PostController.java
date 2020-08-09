5d37d09af8c6b316c99bbbff630d22feb4bdadfd
김준성 <anima94@naver.com>
2020-08-09 22:05:26 +0900

    feat) Post crud setting :sunny:

--- back-end/src/main/java/Anabada/Anabada/controller/PostController.java
+++ back-end/src/main/java/Anabada/Anabada/controller/PostController.java
@@ -1,4 +1,45 @@
 package Anabada.Anabada.controller;
 
+
+import Anabada.Anabada.model.Post;
+import Anabada.Anabada.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> getAllPosts(){
+        return ResponseEntity.ok().body(postService.getAllPosts());
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> createPost(@RequestBody Post post){
+        return ResponseEntity.ok().body(this.postService.createPost(post));
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable long id){
+        return ResponseEntity.ok().body(postService.getPostById(id));
+    }
+
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long id,@RequestBody Post post){
+        post.setId(id);
+        return ResponseEntity.ok().body(this.postService.updatePost(post));
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public HttpStatus deletePost(@PathVariable long id){
+        this.postService.deletePost(id);
+        return HttpStatus.OK;
+    }
 }
